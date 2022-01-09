b2fe4a2e5f2fe5dbb8a758abe611c00fe42690e4
Laura Nepo <lauranepo251@gmail.com>
2022-01-08 17:57:54 -0800

    basic weather info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
 
 export default function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-    console.log("latitude is: " + lat);
-    console.log("longitude is: " + long);
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+    }
+    fetchData();
   }, [lat, long]);
 
   return (
     <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div></div>
+      )}
     </div>
   );
 }
