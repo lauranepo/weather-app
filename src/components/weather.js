53aaeaacd37700212869e3855ba9509afd57dfda
Laura Nepo <lauranepo251@gmail.com>
2022-01-11 18:27:25 -0800

    added some styling

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,20 +4,20 @@ import { Card } from 'semantic-ui-react';
 import moment from "moment";
 
 const WeatherCard = ({ weatherData }) => (
-    <div class='ui centered card'>
-        <Card>
-            <Card.Content>
+    <div className='ui centered card'>
+        <Card className="card">
+            <Card.Content className="card-content">
                 <Card.Header className="header">
                     {weatherData.name}
                 </Card.Header>
-                <p>{moment().format('dddd')}, {moment().format('LL')}</p>
+                <p className="date">{moment().format('dddd')}, {moment().format('LL')}</p>
                 <p>Temperature: {weatherData.main.temp} &deg;F </p>
                 <p>Feels Like: {weatherData.main.feels_like} &deg;F</p>
                 <p>Min: {weatherData.main.temp_min} &deg;F / Max: {weatherData.main.temp_max} &deg;F</p>
                 <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-US')}</p>
                 <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-US')}</p>
                 <p>Description: {weatherData.weather[0].description}</p>
-                <p>Humidity: {weatherData.main.humidity} %</p>
+                <p>Humidity: {weatherData.main.humidity}%</p>
             </Card.Content>
         </Card>
     </div>
